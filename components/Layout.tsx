770c5adae4b34b842db8ae7f512d702962a9e9a6
amir <amir123fg5@gmail.com>
2022-11-14 13:29:07 +0330

    ADD: details in footer and render it in layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { HtmlHTMLAttributes, ReactElement } from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({children}: any) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>apple store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
