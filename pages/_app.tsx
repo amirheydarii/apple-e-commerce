8880c70df17da74969cd5e1328a4680ce77368ae
amir <amir123fg5@gmail.com>
2022-11-15 14:32:58 +0330

    ADD: add Context function to root of project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
